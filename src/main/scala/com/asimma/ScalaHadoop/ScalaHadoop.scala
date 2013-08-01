8b04bac30c272146c0e9b497c046117ae14328a3
Adam Retter <adam.retter@googlemail.com>
2013-08-01 18:36:58 +0100

    [bugfix] Make sure main() is correct signature

--- src/main/scala/com/asimma/ScalaHadoop/ScalaHadoop.scala
+++ src/main/scala/com/asimma/ScalaHadoop/ScalaHadoop.scala
@@ -4,7 +4,9 @@ import org.apache.hadoop.conf.Configured
 import org.apache.hadoop.util.{ToolRunner, Tool}
 
 abstract class ScalaHadoop extends Configured with Tool {
-  def main(args: Array[String]) = ToolRunner.run(this, args)
+  def main(args: Array[String]) {
+    ToolRunner.run(this, args)
+  }
 }
 
 
