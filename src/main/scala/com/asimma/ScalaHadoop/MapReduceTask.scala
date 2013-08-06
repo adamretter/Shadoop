614569c2e21bb030cc0353edd871f550572fabe0
rwalpole <robkwalpole@gmail.com>
2013-08-06 17:50:00 +0100

    [feature] Allow map/reduce tasks which have no mapper and just a reducer

--- src/main/scala/com/asimma/ScalaHadoop/MapReduceTask.scala
+++ src/main/scala/com/asimma/ScalaHadoop/MapReduceTask.scala
@@ -20,15 +20,21 @@ import org.apache.hadoop.mapreduce.{Mapper => HMapper}
 import org.apache.hadoop.mapreduce.{Reducer => HReducer}
 import org.apache.hadoop.mapreduce.Job
 
-case class MapReduceTask[KIN, VIN, KOUT, VOUT](mapper: Mapper[KIN, VIN, _, _],
+case class MapReduceTask[KIN, VIN, KOUT, VOUT](mapper: Option[Mapper[KIN, VIN, _, _]],
                                                combiner: Option[Reducer[_, _, _, _]],
                                                reducer: Option[Reducer[_, _, KOUT, VOUT]],
                                                name: String) {
 
   def initJob(conf: Configuration): Job = {
     val job = new Job(conf, this.name)
-    job.setJarByClass(mapper.getClass)
-    job.setMapperClass(mapper.getClass.asInstanceOf[Class[HMapper[KIN, VIN, _, _]]])
+
+    //job.setJarByClass(mapper.getClass)
+
+    mapper match {
+      case Some(m) =>
+        job.setMapperClass(m.getClass.asInstanceOf[Class[HMapper[KIN, VIN, _, _]]])
+      case None =>
+    }
 
     combiner match {
       case Some(c) =>
@@ -39,66 +45,51 @@ case class MapReduceTask[KIN, VIN, KOUT, VOUT](mapper: Mapper[KIN, VIN, _, _],
     reducer match {
       case Some(r) =>
         job.setReducerClass(r.getClass.asInstanceOf[Class[HReducer[_, _, KOUT, VOUT]]])
-        job.setMapOutputKeyClass(mapper.kType)
-        job.setMapOutputValueClass(mapper.vType)
         job.setOutputKeyClass(r.kType)
         job.setOutputValueClass(r.vType)
-      case None =>
-        job.setOutputKeyClass(mapper.kType)
-        job.setOutputValueClass(mapper.vType)
+
+        mapper match {
+          case Some(m) =>
+            job.setMapOutputKeyClass(m.kType)
+            job.setMapOutputValueClass(m.vType)
+          case None =>
+        }
+
+      case None if(!mapper.isEmpty) =>
+        job.setOutputKeyClass(mapper.get.kType)
+        job.setOutputValueClass(mapper.get.vType)
     }
 
     job
   }
 }
 
-/*
-class MapOnlyTask[KIN, VIN, KOUT, VOUT](
-  mapper: Mapper[KIN, VIN, _, _],
-  name: String)
-  extends MapReduceTask[KIN, VIN, KOUT, VOUT](mapper, None, None, name)    { }
-
-class MapAndReduceTask[KIN, VIN, KOUT, VOUT](
-  mapper: Mapper[KIN, VIN, _, _],
-  reducer: Option[Reducer[_, _, KOUT, VOUT]],
-  name: String)
-  extends MapReduceTask[KIN, VIN, KOUT, VOUT](mapper, None, reducer, name)    { }
-
-class MapCombineReduceTask[KIN, VIN, KOUT, VOUT](
-  mapper: Mapper[KIN, VIN, _, _],
-  combiner: Option[Reducer[_, _, _, _]],
-  name: String)
-  extends MapReduceTask[KIN, VIN, KOUT, VOUT](mapper, combiner, None, name)    { }
-*/
-
 object MapReduceTask {
 
   def apply[KIN, VIN, KOUT, VOUT](
    mapper: Mapper[KIN, VIN, KOUT, VOUT],
    name: String): MapReduceTask[KIN, VIN, KOUT, VOUT] = {
-    apply(mapper, None, None, name)
+    apply(Option(mapper), None, None, name)
   }
 
   def apply[KIN, VIN, KOUT, VOUT](
    mapper: Mapper[KIN, VIN, KOUT, VOUT],
    combiner: Reducer[_, _, _, _],
    name: String): MapReduceTask[KIN, VIN, KOUT, VOUT] = {
-    apply(mapper, Option(combiner), None, name)
+    apply(Option(mapper), Option(combiner), None, name)
   }
-
-  /*
-  def apply[KIN, VIN, KOUT, VOUT, KTMP, VTMP](
-   mapper: Mapper[KIN, VIN, KTMP, VTMP],
-   reducer: Reducer[KTMP, VTMP, KOUT, VOUT],
-   name: String): MapReduceTask[KIN, VIN, KOUT, VOUT] = {
-    apply[KIN, VIN, KOUT, VOUT](mapper, None, Option(reducer), name)
-  }*/
 
   def apply[KIN, VIN, KOUT, VOUT, KTMP, VTMP](
    mapper: Mapper[KIN, VIN, KTMP, VTMP],
    combiner: Reducer[_, _, _, _],
    reducer: Reducer[KTMP, VTMP, KOUT, VOUT],
    name: String): MapReduceTask[KIN, VIN, KOUT, VOUT] = {
-    apply[KIN, VIN, KOUT, VOUT](mapper, Option(combiner), Option(reducer), name)
+    apply[KIN, VIN, KOUT, VOUT](Option(mapper), Option(combiner), Option(reducer), name)
+  }
+
+  def apply[KIN, VIN, KOUT, VOUT](
+     reducer: Reducer[KIN, VIN, KOUT, VOUT],
+     name: String): MapReduceTask[KIN, VIN, KOUT, VOUT] = {
+    apply[KIN, VIN, KOUT, VOUT](None, None, Option(reducer), name)
   }
 }
