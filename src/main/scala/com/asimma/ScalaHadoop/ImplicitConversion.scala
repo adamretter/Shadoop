ce8c41505d0887cabdc6ea311373dc4d38335d84
Adam Retter <adam.retter@googlemail.com>
2013-11-18 12:16:36 +0000

    [doc] Updated license header using maven plugin

--- src/main/scala/com/asimma/ScalaHadoop/ImplicitConversion.scala
+++ src/main/scala/com/asimma/ScalaHadoop/ImplicitConversion.scala
@@ -1,11 +1,11 @@
 /**
- * Copyright 2013
+ * Copyright (C) 2013 Adam Retter (adam.retter@googlemail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// This is inspired by Shadoop 
-// (http://blog.jonhnnyweslley.net/2008/05/shadoop.html) 
+// This is inspired by Shadoop
+// (http://blog.jonhnnyweslley.net/2008/05/shadoop.html)
 package com.asimma.ScalaHadoop
 
 import org.apache.hadoop.io._
